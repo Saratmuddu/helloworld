1e3b37bc84536dd12de2ee9b2f7306ad8bca80c3
saratmuddu <saratmuddu18@gmail.com>
2018-07-24 11:31:26 +0530

    helloworldnode

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World Welcome to node application!');
+}).listen(8080);
